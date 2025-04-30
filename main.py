608ab7ca1428e0cd3a15c1763dfdf2f3e3ddb03a
Adam <adamlor1234@gmail.com>
2025-04-30 17:12:21 +0300

    check_for_tests4

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def my_function(food):
 
 
 # Example usage
-fruits = ["apple", "banana", "cherrryy"]
+fruits = ["apple", "banana", "cherrry"]
 result = my_function(fruits)
 print(result)  # Optional, for seeing the output when you run the script
