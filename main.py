8599f6bebafdf1bcd205b980c08e0f8e902c05dc
Adam <adamlor1234@gmail.com>
2025-04-30 18:10:02 +0300

    check_for_tes2

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def my_function(food):
 
 
 # Example usage
-fruits = ["apple", "bananaa", "cherrry"]
+fruits = ["apple", "bananaa", "chery"]
 result = my_function(fruits)
 print(result)  # Optional, for seeing the output when you run the script
